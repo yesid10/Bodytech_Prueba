e72eff3ae457731483ea4e5ae1aefdd75a128422
yesid10 <yesidvanegas68@gmail.com>
2026-02-05 09:14:53 -0500

    Se eliminan archivos que estaban en deshuso, ej: despues de usar Redux se elimina el contexto

--- Front/src/services/auth.ts
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-export const getCurrentUser = async (token: string) => {
-    const response = await api.post('/me', {}, {
-        headers: { Authorization: `Bearer ${token}` }
-    });
-    return response.data;
-};
